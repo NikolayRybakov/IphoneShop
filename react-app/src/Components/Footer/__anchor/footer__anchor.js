18c67102535b48049ac15c40db881a1fb08b4d13
Lemur <rybakov.nikolai@inbox.ru>
2022-11-23 13:13:32 +0300

    Version 2 React

--- react-app/src/Components/Footer/__anchor/footer__anchor.js
+++
@@ -1,9 +0,0 @@
-import "./_footer__anchor.scss";
-
-const FooterAnchor = () => {
-    return (
-        <a className="footer__anchor" href="#logo">Навверх</a>
-    )
-}
-
-export default FooterAnchor;
